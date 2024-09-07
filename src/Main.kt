3f25b2bde3b033b5e8f31648ef6fa8f4ca4ab74c
Admin <reteamboss@gmail.com>
2024-09-07 14:39:32 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    println("Hello World!")
+
+    operation("1", 'i')
+    operation("234", 'l')
+    operation(1, 'c')
+
+}
+
+fun <T> operation(value: T, ch: Char){
+    when (ch) {
+        'i' -> println("{${value as Int}}")
+        'l' -> println(value as List<*>)
+        's' -> println("[${value as String}]")
+        'c' -> println("(${value as Char})")
+    }
+
 }
